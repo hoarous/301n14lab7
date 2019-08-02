49948c70981851c77f5bd5aa4c355dc2e777003f
Mae-Mae Zhou <semiramis.mei@gmail.com>
2019-08-01 19:03:47 -0700

    copied content from lab6

---
+++ README.md
@@ -0,0 +1,37 @@
+# Project Name
+
+**Author**: Aaron Imbrock, Mae-mae Zhou
+**Version**: 1.0.0
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+```text
+Number and name of feature: 1: Locations
+Estimate of time needed to complete: 2 hr
+Start time: 7:57pm
+Finish time: 
+Actual time needed to complete:
+```
+
+```text
+Number and name of feature: 2: Weather
+Estimate of time needed to complete: 1 hr
+Start time: 12:08am
+Finish time: 
+Actual time needed to complete:
+```
---
+++ server.js
@@ -0,0 +1,61 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const app = express();
+
+// get the specified port from our environmental variables
+// if there is nothing specified, use port 3000
+const PORT = process.env.PORT || 3000;
+
+// tell the server to look in the public folder
+// for any routes or static files such as html, images etc...
+app.use(express.static('./public'));
+app.use(cors);
+
+// Weather. Return data located in data/darksky.json.
+app.get('/weather', (request, response) =>{
+  try{
+    let weatherData = require('./data/darksky.json');
+    let weather = [];//new Weather(weatherData);
+    weatherData.daily.data.forEach((day) =>{
+      weather.push(new Weather(day));
+    });
+    response.send(weather);
+  } catch(error){
+    console.log(`There was an error getting the weather: ${error}`);
+    response.status(500).send('No weather here');
+  }
+});
+
+// located in our data folder
+app.get('/location', (request,response) =>{
+  try{
+    let locationData = require('./data/geo.json');
+    let loc = new Location (locationData.results);
+    response.send(loc);
+  } catch(error){
+    console.log('There was an error getting the location!');
+    response.status(500).send('No location data here');
+  }
+});
+
+function Location(query) {
+  this.data = query;
+  this.search_query = query[0].address_components[0].long_name;
+  this.name = query[0].formatted_address;
+  this.latitude = query[0].geometry.location.lat;
+  this.longitude = query[0].geometry.location.lng;
+}
+
+// TODO: Change to pull weather day objects.
+function Weather(query) {
+  // this.data = query;
+  this.forecast = query.summary;
+  this.time = new Date(query.time*1000).toDateString();
+}
+
+//when we connect to the port, tell us what port we are listening too
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
